20f52793371515a039f5e9e022abf03277aa9bef
Rattlehead90 <strogonov@seznam.cz>
2022-08-15 16:57:38 +0200

    Create Node class with #<=>

---
+++ lib/main.rb
@@ -0,0 +1,15 @@
+# Node class with an attribute for data as well as left and right children
+class Node
+  include Comparable
+  attr_accessor :value, :left_child, :right_child
+
+  def initialize(value, left_child = nil, right_child = nil)
+    @value = value
+    @left_child = left_child
+    @right_child = right_child
+  end
+
+  def <=>(other)
+    value <=> other.value
+  end
+end
