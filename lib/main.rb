19ca6b2f6020aba749e39ef2e6781261b842ff03
Rattlehead90 <strogonov@seznam.cz>
2022-08-16 21:11:16 +0200

    Add MergeSort module, #build_tree, #pretty_print

--- lib/main.rb
+++ lib/main.rb
@@ -13,3 +13,49 @@ class Node
     value <=> other.value
   end
 end
+
+module MergeSort
+  def merge_sort(array)
+    return array unless array.size > 1
+
+    mid = array.size/2
+    a, b, sorted = merge_sort(array[0...mid]), merge_sort(array[mid..-1]), []
+    sorted << (a[0] < b[0] ? a.shift : b.shift) while [a,b].none?(&:empty?)
+    sorted + a + b
+  end
+end
+
+# Tree is a rooted binary tree data structure
+class Tree
+  include MergeSort
+  attr_accessor :array, :processed_array, :root
+
+  def initialize(array)
+    @array = array
+    @processed_array = merge_sort(array).uniq!
+    @root = build_tree(@processed_array)
+  end
+  # e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+  def build_tree(array, start = 0, stop = array.length - 1)
+    return nil if start > stop
+    
+    mid = (start + stop) / 2;
+    root = Node.new(array[mid])
+    root.left_child = build_tree(array, start, mid - 1)
+    root.right_child = build_tree(array, mid + 1, stop)
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end  
+end
+
+test_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+puts test_tree.array.to_s
+puts test_tree.processed_array.to_s
+puts test_tree.pretty_print
