b665c0ef4e000a20ad1f81ba3099658a16a1aae4
Rattlehead90 <strogonov@seznam.cz>
2022-08-19 16:05:33 +0200

    Overhaul, refactor and add order traversals

--- lib/main.rb
+++ lib/main.rb
@@ -1,131 +1,143 @@
-# Node class with an attribute for data as well as left and right children
-class Node
-  include Comparable
-  attr_accessor :value, :left_child, :right_child
-
-  def initialize(value, left_child = nil, right_child = nil)
-    @value = value
-    @left_child = left_child
-    @right_child = right_child
-  end
-
-  def <=>(other)
-    @value <=> other.value
-  end
-end
-
-# Merge sorting algorithm
 module MergeSort
   def merge_sort(array)
     return array unless array.size > 1
 
-    mid = array.size/2
-    a, b, sorted = merge_sort(array[0...mid]), merge_sort(array[mid..-1]), []
-    sorted << (a[0] < b[0] ? a.shift : b.shift) while [a,b].none?(&:empty?)
+    mid = array.size / 2
+    a = merge_sort(array[0...mid])
+    b = merge_sort(array[mid..-1])
+    sorted = []
+    sorted << (a[0] < b[0] ? a.shift : b.shift) while [a, b].none?(&:empty?)
     sorted + a + b
   end
 end
 
-# Tree is a rooted binary tree data structure
+class Node
+  include Comparable
+  attr_accessor :value, :left, :right
+
+  def initialize(value, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+  def <=>(other)
+    value <=> other&.value
+  end
+end
+
 class Tree
   include MergeSort
-  attr_accessor :array, :processed_array, :root
+  attr_accessor :root, :sorted_array
 
   def initialize(array)
-    @array = array
-    @processed_array = merge_sort(array).uniq!
-    @root = build_tree(@processed_array)
+    @sorted_array = merge_sort(array).uniq
+    @root = build_tree(@sorted_array)
   end
-  # e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-  def build_tree(array, start = 0, stop = array.length - 1)
-    return nil if start > stop
-    
-    mid = (start + stop) / 2;
+  def build_tree(array, start = 0, finish = array.length - 1)
+    return nil if start > finish
+
+    mid = (start + finish) / 2
     root = Node.new(array[mid])
-    root.left_child = build_tree(array, start, mid - 1)
-    root.right_child = build_tree(array, mid + 1, stop)
-
+    root.left = build_tree(array, start, mid - 1)
+    root.right = build_tree(array, mid + 1, finish)
     root
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
-    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
-  def insert(root, value)
-    return nil if @processed_array.include?(value)
-    return root = Node.new(value) if root.nil?
+  def insert(value, node = @root)
+    return node = Node.new(value) if node.nil?
 
-    if root.value < value
-      root.right_child = insert(root.right_child, value)
-    else
-      root.left_child = insert(root.left_child, value)
+    if value < node.value
+      node.left = insert(value, node.left)
+    elsif value > node.value
+      node.right = insert(value, node.right)
     end
-    root
+    node
   end
 
-  def delete(root, value)
-    return root if root.nil?
+  def minimum(node)
+    min = node.value
+    until node.left.nil?
+      min = node.left.value
+      node = node.left
+    end
+    min
+  end
 
-    if root.value < value
-      root.right_child = delete(root.right_child, value)
-    elsif root.value > value
-      root.left_child = delete(root.left_child, value)
+  def delete(value, node = @root)
+    return node if node.nil?
+
+    if value < node.value
+      node.left = delete(value, node.left)
+    elsif value > node.value
+      node.right = delete(value, node.right)
     else
-      if root.left_child.nil?
-        return root.right_child
-      elsif root.right_child.nil?
-        return root.left_child
+      if node.left.nil?
+        return node.right
+      elsif node.right.nil?
+        return node.left
       end
-      root.value = minimum(root.right_child)
-      root.right_child = delete(root.right_child, root.value)
-    end
-    root
-  end 
 
-  def minimum(root)
-    minimum_value = root.value
-    until root.left_child.nil?
-      minimum_value = root.left_child.value
-      root = root.left_child
+      node.value = minimum(node.right)
+      node.right = delete(node.right, node.value)
     end
-    minimum_value
+    node
   end
 
-  def find(root, value)
-    return root if root.value == value || root.nil?
-    return find(root.right_child, value) if root.value < value
+  def find(value, node = @root)
+    return node if node.nil? || node.value == value 
+    return find(value, node.right) if node.value < value
 
-    find(root.left_child, value)
+    find(value, node.left)
   end
 
   def level_order
-    result = []
     queue = [@root]
     return nil if @root.nil?
-    returns @array unless block_given?
+    return @sorted_array unless block_given?
 
     until queue.empty?
       current_node = queue.shift
       yield current_node
-      queue.append(current_node.left_child) if current_node.left_child
-      queue.append(current_node.right_child) if current_node.right_child
+      queue.append(current_node.left) if current_node.left
+      queue.append(current_node.right) if current_node.right
     end
   end
+
+  def inorder(node = @root, output = [], &block)
+    return if node.nil?
+
+    inorder(node.left, output, &block)
+    output.push(block_given? ? block.call(node) : node.value)
+    inorder(node.right, output, &block)
+
+    output
+  end
+
+  def preorder(node = @root, output = [], &block)
+    return if node.nil?
+
+    output.push(block_given? ? block.call(node) : node.value)
+    preorder(node.left, output, &block)
+    preorder(node.right, output, &block)
+
+    output
+  end
+
+  def postorder(node = @root, output = [], &block)
+    return if node.nil?
+
+    postorder(node.left, output, &block)
+    postorder(node.right, output, &block)
+    output.push(block_given? ? block.call(node) : node.value)
+
+    output
+  end
 end
-
-test_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-puts test_tree.root.value
-puts test_tree.array.to_s
-puts test_tree.processed_array.to_s
-puts test_tree.pretty_print
-test_tree.insert(test_tree.root, 2)
-test_tree.insert(test_tree.root, 1)
-puts test_tree.pretty_print
-test_tree.delete(test_tree.root, 8)
-puts test_tree.pretty_print
-
-test_tree.level_order { |node| puts node.value }
